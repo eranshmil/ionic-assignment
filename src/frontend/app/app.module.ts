c9d337154874fad469d608ebf2371139bb4478ed
eranshmil <eranshmil@users.noreply.github.com>
2018-10-17 23:50:37 +0300

    generate express project and separate frontend and backend

--- src/frontend/app/app.module.ts
+++ src/frontend/app/app.module.ts
@@ -8,23 +8,14 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  declarations: [MyApp, HomePage],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, HomePage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
-  ]
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+  ],
 })
 export class AppModule {}
