52b7fa181a04a4105b8f81865ce43b0382908866
eranshmil <eranshmil@users.noreply.github.com>
2018-10-20 23:20:19 +0300

    Update src/server/api/api.js

--- src/server/api/api.js
+++ src/server/api/api.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 
 router.use('/url', require('./url/routes'));
+
+// db utils routes
 router.use('/db', require('./db/routes'));
 
 module.exports = router;
