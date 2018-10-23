b961d43b7cca31ff6344eb9bd85f55a2bc8d3ef7
eranshmil <eranshmil@users.noreply.github.com>
2018-10-23 23:06:32 +0300

    fix mongodb environment variable

--- src/server/config/config.js
+++ src/server/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   port: process.env.PORT || 3000,
-  mongoUri: process.env.MONGO_URI || 'mongodb://0.0.0.0:27017/assignment',
+  mongoUri: process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/assignment',
 };
 
 module.exports = config;
