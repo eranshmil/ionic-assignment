42fa2c1121cf15b2df0eff4b0bb9709b6c185872
eranshmil <eranshmil@users.noreply.github.com>
2018-10-24 22:18:39 +0300

    remove unused stuff

--- src/client/providers/socket.provider.ts
+++ src/client/providers/socket.provider.ts
@@ -1,19 +1,14 @@
-import { Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 import { Socket } from 'ng-socket-io';
-
-import { APP_CONFIG_TOKEN, AppConfig } from '../app/app.config';
 
 @Injectable()
 export class SocketProvider {
   readonly getUrlEventName = 'GET_URL';
   readonly sendUrlEventName = 'SEND_URL';
 
-  constructor(
-    @Inject(APP_CONFIG_TOKEN) private _config: AppConfig,
-    private _socket: Socket
-  ) {}
+  constructor(private _socket: Socket) {}
 
   public connect(oldUrl: string, newUrl: string): Observable<string> {
     this._socket.connect();
